97d48c5da64d8a832376c512e43dd5e212bd943b
Luiz Henrique <luizhjramos@outlook.com>
2021-12-11 17:00:07 -0300

    js: eventScrollHeader function

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const eventScrollHeader = () => {
+    const nav = document.getElementById('header')
+
+    if(this.scrollY >= 50) {
+        nav.classList.add("active-header")
+        return
+    }
+
+    nav.classList.remove("active-header")
+}
