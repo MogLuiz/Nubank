617e0f52f71bd02520b527b999a3f9e8abf29878
Luiz Henrique <luizhjramos@outlook.com>
2021-12-11 23:27:20 -0300

    feat: functions for show dropdowns

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// SCROLL HEADER
 const eventScrollHeader = () => {
     const nav = document.getElementById('header')
 
@@ -10,3 +11,33 @@ const eventScrollHeader = () => {
 }
 
 window.addEventListener("scroll", eventScrollHeader)
+
+// DROPDOWN MENU
+const dropdownMenu1 = document.getElementById("dropdown-item-1")
+const dropdownMenu2 = document.getElementById("dropdown-item-2")
+const dropdownMenu3 = document.getElementById("dropdown-item-3")
+
+const menuItem1 = document.getElementById("menu-item-1")
+const menuItem2 = document.getElementById("menu-item-2")
+const menuItem3 = document.getElementById("menu-item-3")
+
+const showDropdown1 = (event) => {
+    event.preventDefault()
+    dropdownMenu1.classList.toggle("active-dropdown-menu")
+    dropdownMenu2.classList.remove("active-dropdown-menu")
+    dropdownMenu3.classList.remove("active-dropdown-menu")
+}
+
+const showDropdown2 = (event) => {
+    event.preventDefault()
+    dropdownMenu1.classList.remove("active-dropdown-menu")
+    dropdownMenu2.classList.toggle("active-dropdown-menu")
+    dropdownMenu3.classList.remove("active-dropdown-menu")
+}
+
+const showDropdown3 = (event) => {
+    event.preventDefault()
+    dropdownMenu1.classList.remove("active-dropdown-menu")
+    dropdownMenu2.classList.remove("active-dropdown-menu")
+    dropdownMenu3.classList.toggle("active-dropdown-menu")
+}
